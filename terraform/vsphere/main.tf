ab7442faaed9c4ddf53d12c01c4e77aa99e5a5f9
Chris Love <clove@cnmconsulting.net>
2015-11-12 20:03:14 +0000

    updating example

--- terraform/vsphere/main.tf
+++ terraform/vsphere/main.tf
@@ -1,77 +1,81 @@
 variable "datacenter" {}
-variable "host" {}
-variable "pool" {}
+variable "cluster" {}
 variable "template" {}
 variable "ssh_user" {}
-variable "ssh_key" {}
 variable "consul_dc" {}
+variable "network_interface_label" {}
+variable "datastore" {}
 
 variable "short_name" {default = "mi"}
 variable "long_name" {default = "microservices-infrastructure"}
 
 variable "control_count" {default = 3}
 variable "worker_count" {default = 2}
-variable "control_cpu" { default = 1 }
-variable "worker_cpu" { default = 1 }
+variable "control_vcpu" { default = 1 }
+variable "worker_vcpu" { default = 1 }
 variable "control_ram" { default = 4096 }
 variable "worker_ram" { default = 4096 }
 
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vcenter_server = "${var.vcenter_server}"
+}
+
 resource "vsphere_virtual_machine" "mi-control-nodes" {
   name = "${var.short_name}-control-${format("%02d", count.index+1)}"
-  image = "${var.template}"
+  domain = "localdomain"
 
   datacenter = "${var.datacenter}"
-  host = "${var.host}"
-  resource_pool = "${var.pool}"
+  cluster = "${var.cluster}"
 
-  cpus = "${var.control_cpu}"
+  vcpu = "${var.control_vcpu}"
   memory = "${var.control_ram}"
 
-  configuration_parameters = {
-    role = "control"
-    ssh_user = "${var.ssh_user}"
-    consul_dc = "${var.consul_dc}"
+  network_interface {
+    label = "${var.network_interface_label}"
   }
 
-  connection = {
-      user = "${var.ssh_user}"
-      key_file = "${var.ssh_key}"
-      host = "${self.ip_address}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [ "sudo hostnamectl --static set-hostname ${self.name}" ]
+  disk {
+    datastore = "${var.datastore}"
+    template =  "${var.template}"
+    size=32
   }
 
   count = "${var.control_count}"
+
+  custom_configuration_parameters = {
+    "role" = "control"
+    "ssh_user" = "${var.ssh_user}"
+    "consul_dc" = "${var.consul_dc}"
+  }
 }
 
 resource "vsphere_virtual_machine" "mi-worker-nodes" {
   name = "${var.short_name}-worker-${format("%03d", count.index+1)}"
-  image = "${var.template}"
+  domain = "localdomain"
 
   datacenter = "${var.datacenter}"
-  host = "${var.host}"
-  resource_pool = "${var.pool}"
+  cluster = "${var.cluster}"
 
-  cpus = "${var.worker_cpu}"
+  vcpu = "${var.worker_vcpu}"
   memory = "${var.worker_ram}"
 
-  configuration_parameters = {
-    role = "worker"
-    ssh_user = "${var.ssh_user}"
-    consul_dc = "${var.consul_dc}"
-  }
-  
-  connection = {
-      user = "${var.ssh_user}"
-      key_file = "${var.ssh_key}"
-      host = "${self.ip_address}"
+  network_interface {
+    label = "${var.network_interface_label}"
   }
 
-  provisioner "remote-exec" {
-    inline = [ "sudo hostnamectl --static set-hostname ${self.name}" ]
+  disk {
+    datastore = "${var.datastore}"
+    template =  "${var.template}"
+    size=32
   }
 
   count = "${var.worker_count}"
+
+  custom_configuration_parameters = {
+    "role" = "worker"
+    "ssh_user" = "${var.ssh_user}"
+    "consul_dc" = "${var.consul_dc}"
+  }
 }
